de364cce3486344c1f57b1074ea9d1d6e589eede
Vibhu Nohria <65294744+Vibhu562@users.noreply.github.com>
2022-01-14 16:54:29 +0530

    Add files via upload

---
+++ Reverse the Array Elements.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a[10000],n;
+    cin >> n;
+    for (int i=1;i<=n;i++)
+    {
+        cin >> a[i];
+    }
+     for (int i=n;i>0;i--)
+    {
+        cout << a[i] << " ";
+    }
+	return 0;
+}
